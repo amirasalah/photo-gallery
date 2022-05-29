db941d5a9211f28bd2bb43c34c9940fd78ac1eac
amira.dh <amera.salah.89@gmail.com>
2022-05-29 17:07:17 +0200

    Make it pretty

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://apis.slstice.com/mock'
-export const SLIDER_INTERVAL = 6000
+export const SLIDER_INTERVAL = 6 * 1000
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -14,11 +14,13 @@ const HomePage = () => {
     if (!currentslideData || isError || isLoading || !data?.data) return null
 
     return (
-        <section className='columns-2 h-screen'>
-            <div className='w-full flex justify-center'>
-                <Image url={data?.data?.response.media.urls.full} description='' />
+        <section className='columns-2 gap-0 h-screen'>
+            <div className='bg-fixed w-full flex justify-center bg-local'>
+                <div className='h-screen bg-gradient-to-b from-stone-100 to-neutral-400'>
+                    <Image url={data?.data?.response.media.urls.full} description='' />
+                </div>
             </div>
-            <div className='w-full'>
+            <div className='w-full h-screen bg-gradient-to-b from-neutral-400 to-stone-100'>
                 <UserDetails firstName={currentslideData.username} lastName='' imageURL='' />
                 <ImageDetails title={currentslideData.title} description={currentslideData.description} />
                 <ImageMetaData likesCount={currentslideData.likes} datePosted={currentslideData.created} />
